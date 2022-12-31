70571134d7aa78cbac147c9a98c9c02048704f71
TeeCreates <99111124+TeeCreates@users.noreply.github.com>
2022-12-30 22:22:48 -0500

    update

---
+++ client/src/Main.js
@@ -0,0 +1,12 @@
+import meemoji from "./assets/meemoji.png";
+
+export const Main = () => {
+  return (
+    <>
+      <img src={meemoji} alt="" />
+      <h1>Hi,</h1>
+      <h1>I'm Thajanah,</h1>
+      <h1>Full-Stack Web Developer</h1>
+    </>
+  );
+};
